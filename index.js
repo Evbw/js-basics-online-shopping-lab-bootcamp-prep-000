34489c579c6a4110ebc3e5533e9e1caec94912f6
Everett <everett.bowline@gmail.com>
2018-02-25 21:57:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function viewCart() {
       
         }
         else if (itemList.length > 2) {
-           itemList[l-1] = "and "
+           itemList[l-1] = "`and ${itemlist[l-1]`"
            itemList = itemList.join(", ")
         }
         
