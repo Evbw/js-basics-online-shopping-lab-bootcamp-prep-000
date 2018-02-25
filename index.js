c7d03b8ce34329c327bf317b644138433e6858e2
Everett <everett.bowline@gmail.com>
2018-02-25 22:32:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,7 +79,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber) {
+  var test
+  if (test) {
     console.log("Sorry, we don't have a credit card on file for you.")
     return false
   }
