653a6a12aa0e48b87989b58565e00043bc2574fb
Everett <everett.bowline@gmail.com>
2018-02-25 22:18:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber !== null) {
     console.log("Sorry, we don't have a credit card on file for you")
+    return
   }
   
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
